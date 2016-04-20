4e2265c622dab4448507329defbc32f9b99d409a
Edson Martins <edsonmartins2005@gmail.com>
2016-04-20 09:25:00 -0300

    Update copyright.

--- anteros-ui-controls-android/src/main/java/br/com/anteros/android/ui/controls/gestureImageView/GestureImageView.java
+++ anteros-ui-controls-android/src/main/java/br/com/anteros/android/ui/controls/gestureImageView/GestureImageView.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package br.com.anteros.android.ui.controls.gestureImageView;
 
 import java.io.InputStream;
